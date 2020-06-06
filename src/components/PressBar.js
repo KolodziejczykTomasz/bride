ca06e1d0320844c5fc7c17cf00f5f87e5d0da90a
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-06-06 21:37:55 +0200

    Corrected code in component  PressBar

--- src/components/PressBar.js
+++ src/components/PressBar.js
@@ -3,14 +3,30 @@ import PressItem from './PressItem';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  display: grid;
+  display: flexbox;
+`;
+
+const WrapperItem = styled.div`
+  width: 20%;
 `;
 
 const PressBar = () => (
   <Wrapper>
-    <di>
+    <WrapperItem>
       <PressItem />
-    </di>
+    </WrapperItem>
+    <WrapperItem>
+      <PressItem />
+    </WrapperItem>
+    <WrapperItem>
+      <PressItem />
+    </WrapperItem>
+    <WrapperItem>
+      <PressItem />
+    </WrapperItem>
+    <WrapperItem>
+      <PressItem />
+    </WrapperItem>
   </Wrapper>
 );
 
--- src/components/PressItem.js
+++ src/components/PressItem.js
@@ -1,30 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import press1 from '../assets/images/press/bc_2_2019.jpg';
 
+const Wrapper = styled.div``;
 
 const PressItem = () => (
-    <div className="card">
-        <div class="card-content">
-            <p className="title">
-                “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
-    </p>
-            <p className="subtitle">
-                Jeff Atwood
-    </p>
-        </div>
-        <footer className="card-footer">
-            <p className="card-footer-item">
-                <span>
-                    View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                </span>
-            </p>
-            <p className="card-footer-item">
-                <span>
-                    Share on <a href="#">Facebook</a>
-                </span>
-            </p>
-        </footer>
+  <Wrapper className="card">
+    <div class="card-content">
+      <p className="title">
+        <img src={press1} alt="press" />
+      </p> 
     </div>
-
+  </Wrapper>
 );
 
 export default PressItem;
