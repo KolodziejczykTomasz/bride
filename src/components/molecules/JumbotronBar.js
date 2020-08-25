50268301fb36be6d3685de49447212b8fb0fe5bc
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-08-25 21:32:49 +0200

    Corrected code in JumbotronBar

--- src/components/molecules/JumbotronBar.js
+++ src/components/molecules/JumbotronBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 import styled from 'styled-components';
 import Image from 'assets/images/slider/slider2.jpg';
 
@@ -12,16 +12,9 @@ const JumbotronBar = (props) => (
     >
       <div style={{ marginTop: '15rem' }}>
         <h1 className="display-3">Ślub z klasą!</h1>
-        <p className="lead">
-          Zaplanuj z nami swój wymarzony ślub.
-        </p>
+        <p className="lead">Zaplanuj z nami swój wymarzony ślub.</p>
         <hr className="my-2" />
-        <p>
-          - spokojnie i bez paniki.
-        </p>
-        <p className="lead">
-          <Button color="primary">Zobacz więcej</Button>
-        </p>
+        <p>- spokojnie i bez paniki.</p>
       </div>
     </Jumbotron>
   </Wrapper>
