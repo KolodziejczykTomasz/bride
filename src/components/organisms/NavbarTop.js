e66803fee713c16c42cb394d6465fe29a3c82a46
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-09-21 21:52:49 +0200

    Corrected code in NavbarTop

--- src/components/organisms/NavbarTop.js
+++ src/components/organisms/NavbarTop.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Navbar } from 'react-bootstrap';
 import { FaPhone } from 'react-icons/fa';
 import BarndMark from 'assets/images/logotypy/brendmark.jpg';
-
 import './NavbarTop.css';
 
 const NavbarTop = () => (
-  <>
-    <div id="navbarHeader">
-      <Navbar.Brand id="navbarHeaderBrand">
-        <img src={BarndMark} alt="BarndMark" id="navbarHeaderBrandPhoto" />
-      </Navbar.Brand>
-      <div id="navbarHeaderPhone">
-        <Navbar.Text style={{ backgroundColor: '#fff', fontSize: '12' }}>
-          <FaPhone style={{ marginRight: '10px' }} />
-          +48 123 456 789
-        </Navbar.Text>
+  <Fragment>
+    <div class="navbar-brand" id="headerNavbar">
+      <div>
+        <span id="headerNavbarSpan">Kontakt z redakcją portalu</span>
+        <span id="headerNavbarSpanSecond"> "ślub z klasą"</span>
+        <FaPhone style={{ marginRight: '10px' }} />
+        +48 123 456 789
       </div>
     </div>
-    <nav class="navbar navbar-expand-md sticky-top">
+
+    <div class="navbar navbar-expand-md navbar-light sticky-top" id="wrapperNavbar">
       <div class="navbar-toggler-right">
         <button
           class="navbar-toggler"
@@ -33,43 +30,46 @@ const NavbarTop = () => (
           <span class="navbar-toggler-icon"></span>
         </button>
       </div>
-      <NavLink to="/">
-        <Navbar.Brand
-          id="navbarHeaderSecondBrand"     
-        >
-          Ślub z Klasą
+      <NavLink to="/" style={{ marginRight: 20 }}>
+        <Navbar.Brand style={{ width: 'auto', textTransform: 'uppercase', marginRight: 20 }}>
+          <img src={BarndMark} alt="BarndMark" /> Ślub z Klasą
         </Navbar.Brand>
       </NavLink>
       <nav class="collapse navbar-collapse flex-column " id="navbar">
         <ul class="navbar-nav  w-100 justify-content-center px-3">
-          <li class="nav-item dropdown">
-            <NavLink id="navlink" to="/accesorieslist">
+          <li class="nav-link">
+            <NavLink class="button" to="/" id="navbutton">
+              Home
+            </NavLink>
+          </li>
+          <li class="nav-link">
+            <NavLink class="button" to="/accesorieslist" id="navbutton">
               Akcesoria
             </NavLink>
           </li>
-          <li class="nav-item dropdown">
-            <NavLink id="navlink" to="/presslist">
+          <li class="nav-link">
+            <NavLink class="button" to="/presslist" id="navbutton">
               Prasa
             </NavLink>
           </li>
-          <li class="nav-item dropdown">
-            <NavLink id="navlink" to="/company">
+          <li class="nav-link">
+            <NavLink class="button" to="/company" id="navbutton">
               Katalog firm
             </NavLink>
           </li>
-          <li class="nav-item dropdown">
-            <NavLink id="navlink" to="/fairlist">
+          <li class="nav-link">
+            <NavLink class="button" to="/fairlist" id="navbutton">
               Targi
             </NavLink>
           </li>
-          <li class="nav-item dropdown">
-            <NavLink id="navlink" to="/contact">
+          <li class="nav-link">
+            <NavLink class="button" to="/contact" id="navbutton">
               Kontakt
             </NavLink>
           </li>
         </ul>
       </nav>
-    </nav>
-  </>
+    </div>
+  </Fragment>
 );
 export default NavbarTop;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './views/Root';
 import 'bulma/css/bulma.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@material/react-card/dist/card.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
